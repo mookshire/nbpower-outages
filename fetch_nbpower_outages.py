0ad6a00c618e56504767ca86f4e6585629a46ae3
Merrill Spencer <merrillspencer@live.ca>
2025-06-17 15:18:10 -0300

    Update script to group outages by region

--- fetch_nbpower_outages.py
+++ fetch_nbpower_outages.py
@@ -1,7 +1,8 @@
 import requests
 import json
+from collections import defaultdict
 
-url = "https://services1.arcgis.com/nXhKU3TMjpIZsCx0/arcgis/rest/services/PublicOutageFC_Prod/FeatureServer/0/query"
+url = "https://services1.arcgis.com/nXhKU3TMjpIZsCx0/arcgis/rest/services/PublicOutageFC_Prod/FeatureServer/1/query"
 params = {
     "where": "1=1",
     "outFields": "*",
@@ -9,15 +10,27 @@ params = {
 }
 
 response = requests.get(url, params=params)
-data = response.json().get("features", [])
+data = response.json()
 
-extracted = []
-for feature in data:
+# Group outages by region name
+regions = defaultdict(lambda: {"outages": 0, "customers_affected": 0})
+
+for feature in data.get("features", []):
     attr = feature.get("attributes", {})
-    extracted.append({
-        "customers_affected": attr.get("CustEff"),
-        "outages": attr.get("NoOfOutages")
-    })
+    region = attr.get("RegionName", "Unknown")
+    regions[region]["outages"] += attr.get("NoOfOutages", 0)
+    regions[region]["customers_affected"] += attr.get("CustEff", 0)
 
+# Convert to clean list
+output = [
+    {
+        "region": region,
+        "outages": info["outages"],
+        "customers_affected": info["customers_affected"]
+    }
+    for region, info in regions.items()
+]
+
+# Save to file
 with open("outages.json", "w") as f:
-    json.dump(extracted, f, indent=2)
+    json.dump(output, f, indent=2)
