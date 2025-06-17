6add0980f2be89451e2e765f922497b90d1e8561
mookshire <merrillspencer@live.ca>
2025-06-17 14:56:27 -0300

    Update fetch_nbpower_outages.py

--- fetch_nbpower_outages.py
+++ fetch_nbpower_outages.py
@@ -1,8 +1,7 @@
-
 import requests
 import json
 
-url = "https://services1.arcgis.com/nXhOU3TMjpIZsCx0/arcgis/rest/services/PublicOutageFC_Prod/FeatureServer/0/query"
+url = "https://services1.arcgis.com/nXhKU3TMjpIZsCx0/arcgis/rest/services/PublicOutageFC_Prod/FeatureServer/0/query"
 params = {
     "where": "1=1",
     "outFields": "*",
@@ -10,17 +9,15 @@ params = {
 }
 
 response = requests.get(url, params=params)
-data = response.json()
+data = response.json().get("features", [])
 
-# Extract only the relevant fields
 extracted = []
-for feature in data.get("features", []):
+for feature in data:
     attr = feature.get("attributes", {})
     extracted.append({
         "customers_affected": attr.get("CustEff"),
-        "outages": attr.get("NoOfOutages"),
+        "outages": attr.get("NoOfOutages")
     })
 
-# Save to outages.json
 with open("outages.json", "w") as f:
     json.dump(extracted, f, indent=2)
